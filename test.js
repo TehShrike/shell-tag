d7cfb47b58f2195a9fe5007377058ccff6c52489
Josh Duff <me@JoshDuff.com>
2017-04-22 11:06:50 -0500

    Testing with a tab indented command

--- test.js
+++ test.js
@@ -9,6 +9,16 @@ test('running echo', t => {
 	t.end()
 })
 
+test('indented lines', t => {
+	const str = 'say "what"'
+	const output = sh`
+		echo ${str}
+	`
+
+	t.equal(output.trim(), 'say "what"')
+	t.end()
+})
+
 test('exit code 1', t => {
 	t.throws(() => sh`node ./test-helper/exit-code-1.js`, /failed/)
 
