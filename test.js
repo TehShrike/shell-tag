b7c304a9c778300328d4b0a3b81a5a67875e3e64
Josh Duff <me@JoshDuff.com>
2018-07-08 11:41:22 -0500

    zora instead of tape

--- test.js
+++ test.js
@@ -1,26 +1,22 @@
-const test = require('tape')
-const sh = require('./')
+const test = require(`zora`)
+const sh = require(`./`)
 
-test('running echo', t => {
-	const str = 'say "what"'
-	const output = sh`echo ${str}`
+test(`running echo`, t => {
+	const str = `say "what"`
+	const output = sh`echo ${ str }`
 
-	t.equal(output.trim(), 'say "what"')
-	t.end()
+	t.equal(output.trim(), `say "what"`)
 })
 
-test('indented lines', t => {
-	const str = 'say "what"'
+test(`indented lines`, t => {
+	const str = `say "what"`
 	const output = sh`
-		echo ${str}
+		echo ${ str }
 	`
 
-	t.equal(output.trim(), 'say "what"')
-	t.end()
+	t.equal(output.trim(), `say "what"`)
 })
 
-test('exit code 1', t => {
+test(`exit code 1`, t => {
 	t.throws(() => sh`node ./test-helper/exit-code-1.js`, /failed/)
-
-	t.end()
 })
